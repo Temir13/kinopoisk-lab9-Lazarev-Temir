adaa1210c3a4e201af0092bb86ddf9b38fd6c31e
Temir <31662@iitu.edu.kz>
2022-04-09 23:26:40 +0600

    update footer skeleton added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,14 @@ const Footer = () =>{
             <div className='footerBlock'>
                 <div className='footerContainer'>
                     <div className='footerSocialLinksUp'>
-                        <div className='footerIconsUp'>footerIconsUp</div>
+                        <div className='footerIconsUp'>
+                            <a href="https://vk.com/kinopoisk" className='footerIconVK'></a>
+                            <a href="https://www.facebook.com/kinopoisk" className='footerIconFB'></a>
+                            <a href="https://twitter.com/kinopoiskru" className='footerIconTW'></a>
+                            <a href="https://telegram.me/kinopoisk" className='footerIconTG'></a>
+                            <a href="https://www.instagram.com/kinopoisk/" className='footerIconINS'></a>
+                            <a href="https://www.youtube.com/user/kinopoisk" className='footerIconYT'></a>
+                        </div>
                         <div className='footerBlogUp'>footerBlogUp</div>
                         <div className='footerAppLinksUpContainer'>footerAppLinksUpContainer</div>
                     
