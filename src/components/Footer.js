f9529e114a5c6838c539fe23e434b5cc973907dc
Temir <31662@iitu.edu.kz>
2022-04-10 18:13:05 +0600

    update footer final

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,12 +14,41 @@ const Footer = () =>{
                             <a href="https://www.instagram.com/kinopoisk/" className='footerIconINS'></a>
                             <a href="https://www.youtube.com/user/kinopoisk" className='footerIconYT'></a>
                         </div>
-                        <div className='footerBlogUp'>footerBlogUp</div>
-                        <div className='footerAppLinksUpContainer'>footerAppLinksUpContainer</div>
+                        <div className='footerBlogsUp'>
+                            <a className='footerBlogVacancy'>Вакансии</a>
+                            <a className='footerBlogCommercial'>Реклама</a>
+                            <a className='footerBlogApproval'>Соглашение</a>
+                            <a className='footerBlogReference'>Справка</a>
+                            <a className='footerBlogBlog'>Блог</a>
+                            <a className='footerBlogResearch'>Участие в исследованиях</a>
+                            <a className='footerBlogProposal'>Предложения</a>
+                            <a className='footerBlogHelpDesk'>Служба поддержки</a>
+                        </div>
+                    <div className='footerAppLinksUpContainer'>
+                        <a className='footerAppStore'></a>
+                        <a className='footerGooglePlay'></a>
+                        <a className='footerAppGallery'></a>
+                    </div>
                     
-                    </div>  
+                </div>  
                     <div className='footerLinksDownContainer'>
-                        <div className='footerLinksDownBlock'>footerLinksDownBlock</div>
+                        <div className='footerLinksDownBlock'>
+                            <div className='footerDownYear'>© 2003 — 2022, 
+                                <a className='footerDownKPA'>  Кинопоиск  </a>
+                                <a className='footerDown18A'>  18+</a>
+                            </div>
+                            <div className='footerDownLink'>
+                                <a className='footerDownTeleProgramm'>Телепрограмма</a>
+                                <a className='footerDownMusic'>Музыка</a>
+                                <a className='footerDowsPlayBill'>Афиша</a>
+                            </div>
+                            <div className='footerDownProject'>Проект Студента:
+                                <a href='https://instagram.com/lazarev_temir?igshid=YmMyMTA2M2Y=' className='footerDownLazarus'> Темира Лазарева</a>
+
+
+                            </div>
+
+                        </div>
                     </div>            
                 </div>
             </div>
