b82948a409b744bb406510ea0239d1d45e23b3c4
Temir <31662@iitu.edu.kz>
2022-04-11 21:24:16 +0600

    uodate fff

--- src/App.js
+++ src/App.js
@@ -2,24 +2,14 @@ import './App.css';
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
-import Aside from './components/Aside';
-
-
-
-
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Aside/>
       <Body/>
-      <Footer/>
-      
- 
-   
-      
+      <Footer/>      
     </div>
   );
 }
